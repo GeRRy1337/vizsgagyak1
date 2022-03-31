ecf7571413b6bdcee04af5e13ed802bce150bbe7
GeRRy1337 <mogema2002@gmail.com>
2022-03-31 12:27:12 +0200

    style

--- view/jelentkezes.php
+++ view/jelentkezes.php
@@ -1,8 +1,8 @@
 <body class="bg-danger">
-<div class="container">
+<div class="container cont">
     <h1>Futóverseny jelentkezési lap</h1>
-    <div class="row bg-light border rounded border-2 border-primary">
-        <div class="col-4 ">
+    <div class="regisztracio bg-light border rounded border-2 border-primary">
+        <div class="col-4">
             <h2>Adatok</h2>
             <form method="Post">
                 <div>
@@ -29,7 +29,7 @@
         </div>
         <div class="col-6">
             <h2>Fontos</h2>
-            <p></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ligula et enim dictum blandit. Maecenas convallis ultrices est, id ultricies turpis rutrum aliquet. Nunc laoreet aliquam justo, sed pulvinar neque vestibulum ut. Vestibulum ut convallis quam. Suspendisse pharetra fringilla quam, at fermentum mauris rhoncus ut. Quisque pellentesque nunc quis ante ultricies dignissim nec vel dolor. Sed vehicula orci odio, non facilisis nunc iaculis et. Integer ut erat ante. Nunc placerat, ex id viverra congue, felis orci cursus augue, vel sollicitudin ipsum ipsum quis erat. Vestibulum eu est quam. Integer scelerisque dui dignissim, congue justo in, accumsan eros. Aenean maximus sit amet magna at dignissim. In eu lacus auctor, cursus massa at, fringilla urna. Praesent blandit gravida libero, at efficitur turpis tempor at.</p>
         </div>
         <div class="col-2">
             <div class="bg-danger rounded">
